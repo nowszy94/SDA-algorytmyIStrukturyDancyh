6bd2917346d86242cd835c12e610456b989a4a22
nowszy94 <nowszy94@gmail.com>
2017-02-14 21:03:44 +0100

    Added count sort

---
+++ src/algorytmy/sort/CountSort.java
@@ -0,0 +1,37 @@
+package algorytmy.sort;
+
+import java.util.Arrays;
+
+/**
+ * Created by Szymon on 2017-02-14.
+ */
+public class CountSort {
+
+    public static void main(String[] args) {
+        CountSort countSort = new CountSort();
+        int[] array = {1,2,3,1,2,2,-1,-1,-2,-3,1,2,3,3,3,3,3,2,2,2,1,1,1,2,2,2,-1};
+        System.out.println(Arrays.toString(countSort.ascSort(array)));
+    }
+
+    public int[] ascSort(int[] array) {
+        int max = SortUtils.max(array);
+        int min = SortUtils.min(array);
+        int range = max - min + 1;
+        int[] rangeArray = new int[range];
+        for(int i = 0 ; i < array.length; i++) {
+            rangeArray[array[i] - min]++;
+        }
+        int index = 0;
+        for (int i = 0; i < rangeArray.length; i++) {
+            for (int j = 0; j < rangeArray[i]; j++) {
+                array[index] = i + min;
+                index++;
+            }
+        }
+        return array;
+    }
+
+//    public int[] descSort(int[] array) {
+//        return null;
+//    }
+}
--- src/algorytmy/sort/SortUtils.java
+++ src/algorytmy/sort/SortUtils.java
@@ -6,4 +6,24 @@ public class SortUtils {
         array[index1] = array[index2];
         array[index2] = tmp;
     }
+
+    public static int min(int[] array) {
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    public static int max(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
 }
